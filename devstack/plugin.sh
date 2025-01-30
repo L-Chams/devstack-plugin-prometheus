c0c05ea8ebddd26867a0f65af21c7e0f6c98249a
Chandan Kumar (raukadah) <chkumar@redhat.com>
2025-01-30 13:01:22 +0530

    Add devStack plugin config for node_exporter
    Change-Id: Ifeb2e505059927d692abb209f29fa27e10ff2f5d
    Signed-off-by: Chandan Kumar (raukadah) <chkumar@redhat.com>

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -6,6 +6,7 @@ set -o xtrace
 
 echo_summary "devstack-plugin-prometheus's plugin.sh was called..."
 . $DEST/devstack-plugin-prometheus/devstack/lib/prometheus
+. $DEST/devstack-plugin-prometheus/devstack/lib/node_exporter
 
 # Show all of defined environment variables
 (set -o posix; set)
@@ -51,5 +52,49 @@ if is_service_enabled prometheus; then
         # no-op
         echo_summary "Cleaning prometheus service"
         cleanup_prometheus
+    fi
+fi
+
+## Node Exporter
+if is_service_enabled node_exporter; then
+    if [[ "$1" == "source" ]]; then
+        # Initial source of lib script
+        source $(dirname "$0")/lib/node_exporter
+    fi
+
+    if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
+        # Set up system services
+        echo_summary "Configuring system services node exporter"
+        pre_install_node_exporter
+
+    elif [[ "$1" == "stack" && "$2" == "install" ]]; then
+        # Perform installation of service source
+        echo_summary "Installing node exporter"
+        install_node_exporter
+
+    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+        # Configure after the other layer 1 and 2 services have been configured
+        echo_summary "Configuring node_exporter"
+        init_node_exporter
+        echo_summary "Starting node exporter service"
+        start_node_exporter
+        echo_summary "Give time to node_exporter to push metrics"
+        wait_for_data
+        check_data
+    fi
+
+    if [[ "$1" == "unstack" ]]; then
+        # Shut down node exporter services
+        # no-op
+        echo_summary "Stoping node_exporter service"
+        stop_node_exporter
+    fi
+
+    if [[ "$1" == "clean" ]]; then
+        # Remove state and transient data
+        # Remember clean.sh first calls unstack.sh
+        # no-op
+        echo_summary "Cleaning node exporter service"
+        cleanup_node_exporter
     fi
 fi
