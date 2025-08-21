8bca306129e942f7026f8090853cf8419804cdf4
Leonie Chamberlin-Medd <leonie@stackhpc.com>
2025-08-21 14:57:12 +0000

    Developmental openstack_exporter scripts

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -7,6 +7,7 @@ set -o xtrace
 echo_summary "devstack-plugin-prometheus's plugin.sh was called..."
 . $DEST/devstack-plugin-prometheus/devstack/lib/prometheus
 . $DEST/devstack-plugin-prometheus/devstack/lib/node_exporter
+. $DEST/devstack-plugin-prometheus/devstack/lib/openstack_exporter
 . $DEST/devstack-plugin-prometheus/devstack/lib/podman_exporter
 . $DEST/devstack-plugin-prometheus/devstack/lib/observabilityclient
 
@@ -104,6 +105,52 @@ if is_service_enabled node_exporter; then
         # no-op
         echo_summary "Cleaning node exporter service"
         cleanup_node_exporter
+    fi
+fi
+
+## Openstack Exporter
+if is_service_enabled openstack_exporter; then
+    if [[ "$1" == "source" ]]; then
+        # Initial source of lib script
+        source $(dirname "$0")/lib/openstack_exporter
+    fi
+
+    if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
+        # Set up system services
+        echo_summary "Configuring system services openstack exporter"
+        pre_install_openstack_exporter
+
+    elif [[ "$1" == "stack" && "$2" == "install" ]]; then
+        # Perform installation of service source
+        echo_summary "Installing openstack exporter"
+        install_openstack_exporter
+
+    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+        # Configure after the other layer 1 and 2 services have been configured
+        echo_summary "Configuring openstack_exporter"
+        init_openstack_exporter
+        echo_summary "Starting openstack exporter service"
+        start_openstack_exporter
+        echo_summary "Give time to openstack_exporter to push metrics"
+        wait_for_data
+        check_data_openstack_exporter
+    fi
+
+    if [[ "$1" == "unstack" ]]; then
+        # Shut down openstack exporter services
+        # no-op
+        echo_summary "Stoping openstack_exporter service"
+        stop_openstack_exporter
+        echo_summary "Cleaning openstack exporter service"
+        cleanup_open_exporter
+    fi
+
+    if [[ "$1" == "clean" ]]; then
+        # Remove state and transient data
+        # Remember clean.sh first calls unstack.sh
+        # no-op
+        echo_summary "Cleaning openstack exporter service"
+        cleanup_openstack_exporter
     fi
 fi
 
